479e3cb266c832434d0439cc222aed7db8517e09
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-04 11:15:59 +0200

    Configured the options window.

---
+++ src/com/barosanu/view/ColorTheme.java
@@ -0,0 +1,10 @@
+package com.barosanu.view;
+
+/**
+ * Created by Arek on 04.04.2020.
+ */
+public enum ColorTheme {
+    LIGHT,
+    DEFAULT,
+    DARK;
+}
---
+++ src/com/barosanu/view/FontSize.java
@@ -0,0 +1,10 @@
+package com.barosanu.view;
+
+/**
+ * Created by Arek on 04.04.2020.
+ */
+public enum FontSize {
+    SMALL,
+    MEDIUM,
+    BIG;
+}
