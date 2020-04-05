54012048d6941784d1b2c3dee44fb24b33f41d52
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-05 12:23:21 +0200

    JavaMail and Email account setup.

---
+++ src/com/barosanu/controller/EmailLoginResult.java
@@ -0,0 +1,11 @@
+package com.barosanu.controller;
+
+/**
+ * Created by Arek on 05.04.2020.
+ */
+public enum EmailLoginResult {
+    SUCCESS,
+    FAILED_BY_CREDENTIALS,
+    FAILED_BY_NETWORK,
+    FAILED_BY_UNEXPECTED_ERROR;
+}
---
+++ src/com/barosanu/controller/services/LoginService.java
@@ -0,0 +1,23 @@
+package com.barosanu.controller.services;
+
+import com.barosanu.EmailManager;
+import com.barosanu.controller.EmailLoginResult;
+import com.barosanu.model.EmailAccount;
+
+/**
+ * Created by Arek on 05.04.2020.
+ */
+public class LoginService {
+
+    EmailAccount emailAccount;
+    EmailManager emailManager;
+
+    public LoginService(EmailAccount emailAccount, EmailManager emailManager) {
+        this.emailAccount = emailAccount;
+        this.emailManager = emailManager;
+    }
+
+    public EmailLoginResult login(){
+
+    }
+}
