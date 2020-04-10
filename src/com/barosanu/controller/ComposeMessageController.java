1b958d9d2c7ca65a4f0e75ce27e6ccc4ace4291f
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-10 16:11:43 +0200

    Compose messages layout.

---
+++ src/com/barosanu/controller/ComposeMessageController.java
@@ -0,0 +1,36 @@
+package com.barosanu.controller;
+
+import com.barosanu.EmailManager;
+import com.barosanu.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+
+/**
+ * Created by Arek on 10.04.2020.
+ */
+public class ComposeMessageController extends BaseController{
+
+    @FXML
+    private TextField recipientTextFIeld;
+
+    @FXML
+    private TextField subjectTextField;
+
+    @FXML
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void sendButtonAction() {
+        System.out.println(htmlEditor.getHtmlText());
+        System.out.println("send button!");
+    }
+
+    public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+}
