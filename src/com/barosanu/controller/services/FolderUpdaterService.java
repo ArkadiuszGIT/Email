fc4f51eb9f076f0f8f0439facc95bc7a6baf39d3
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-11 19:27:13 +0200

    Add folder icons.

--- src/com/barosanu/controller/services/FolderUpdaterService.java
+++ src/com/barosanu/controller/services/FolderUpdaterService.java
@@ -6,12 +6,14 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class FolderUpdaterService extends Service {
 
     private List<Folder> folderList;
-
+   // private List<Folder> folderList = new ArrayList<Folder>();
     public FolderUpdaterService(List<Folder> folderList) {
         this.folderList = folderList;
     }
@@ -25,11 +27,25 @@ public class FolderUpdaterService extends Service {
                 for (;;){
                     try {
                         Thread.sleep(5000);
-                        for (Folder folder: folderList){
+                        //for (Folder folder: folderList){
+                        //    if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen()){
+                       //        folder.getMessageCount();
+                       //    }
+                       // }
+
+                        // In other cases -> java.util.concurrentmodificationexception
+                        for( int i = 0; i < folderList.size(); i++ )
+                        {
+                            Folder folder = folderList.get( i );
                             if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen()){
                                 folder.getMessageCount();
                             }
                         }
+//                        for (Iterator<Folder> iterator = folderList.iterator(); iterator.hasNext();) {
+//                            Folder folder = iterator.next();
+//                            if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen())
+//                                folder.getMessageCount();
+//                        }
                     } catch (Exception e){
                         e.printStackTrace();
                     }
